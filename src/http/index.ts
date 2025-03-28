49d59a82f48354bb38260a88389811fd7d232cd3
darknesses <weirdest.developer@gmail.com>
2025-03-28 20:57:06 +0500

    network updates

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const $host = axios.create({
-    baseURL: 'http://localhost:3000/'
+    baseURL: import.meta.env.VITE_SERVER_URL
 });
 
 const $authHost = axios.create({
-    baseURL: 'http://localhost:3000/'
+    baseURL: import.meta.env.VITE_SERVER_URL
 });
 
 const authInterceptor = config => {

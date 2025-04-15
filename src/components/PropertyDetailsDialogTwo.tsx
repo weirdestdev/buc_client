63d7193cad64d44ae1e596d7131fc335433bdc2d
darknesses <weirdest.developer@gmail.com>
2025-04-15 20:39:59 +0500

    asdg

--- src/components/PropertyDetailsDialogTwo.tsx
+++ src/components/PropertyDetailsDialogTwo.tsx
@@ -86,7 +86,7 @@ function PropertyDetailsDialog({
   return (
     <>
       <Dialog open={open} onOpenChange={onOpenChange}>
-        <DialogContent className="sm:max-w-[700px] max-h-screen overflow-y-auto pb-4 sm:pb-2">
+        <DialogContent className="sm:max-w-[700px] max-h-screen overflow-y-auto pb-[70px] sm:pb-2">
           <DialogHeader>
             <DialogTitle className="text-xl font-display">{property.name}</DialogTitle>
             <DialogDescription className="flex items-center text-muted-foreground">

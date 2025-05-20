490468ffe99459fb3ce4045e9c1a2ff56ac364d2
Можаева Наталья <natalyamozhaeva1107@gmail.com>
2025-05-20 12:47:31 +0500

    asd

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,28 +1,26 @@
 import axios from 'axios';
 
 const $host = axios.create({
-    baseURL: import.meta.env.VITE_SERVER_URL
+  baseURL: import.meta.env.VITE_SERVER_URL
 });
 
 const $authHost = axios.create({
-    baseURL: import.meta.env.VITE_SERVER_URL
+  baseURL: import.meta.env.VITE_SERVER_URL
 });
 
 const authInterceptor = config => {
-    // Если admin-token есть, используем его, иначе обычный token
-    const adminToken = localStorage.getItem('admin-token');
-    const userToken = localStorage.getItem('token');
-    const token = adminToken || userToken;
-    if (token) {
-      config.headers.authorization = `Bearer ${token}`;
-    }
-    return config;
-  };
-  
+  const token = localStorage.getItem('admin-token') || localStorage.getItem('token');
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+};
+
 
 $authHost.interceptors.request.use(authInterceptor);
 
 export {
-    $host,
-    $authHost
+  $host,
+  $authHost
 }
